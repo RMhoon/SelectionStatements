05c45170a274f77dc73c7ece81beed0bee7d4f01
RMhoon <r_mhoon@hotmail.com>
2021-02-23 21:53:35 -0500

    Added a while loop to make a game continous, removed 4th condition

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -9,17 +9,25 @@ namespace SelectionStatements
             var r = new Random();
             int favNumber = r.Next(1, 1000);
 
-            Console.WriteLine("Enter in a number.  Try and guess my favorite number 1 to 1,000");
-            var userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter in a number.  Try and guess my favorite number 1 to 1,000.  Enter exit to end game.");
+            var input = Console.ReadLine();
 
-            if (userNumber > favNumber)
-                Console.WriteLine("The number is too high.");
-            else if (userNumber < favNumber)
+            while (input.ToLower() != "exit")
+            {
+                var userNumber = int.Parse(input);
+
+                if (userNumber > favNumber)
+                    Console.WriteLine("The number is too high.");
+                else if (userNumber < favNumber)
                     Console.WriteLine("The number is too low.");
-            else if (userNumber == favNumber)
-                Console.WriteLine("You won.  You have guessed my number");
-            else
-                Console.WriteLine("Nevermind");
+                else
+                    Console.WriteLine("Nevermind");
+
+                Console.WriteLine("Enter in another number or enter exit to end game.");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("You have exited the game. Good bye.");
 
         }
     }
